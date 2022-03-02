973e1f16bcaa52f1d63ecada82a781e1145cdf9e
Toxic18 <49251120+Toxic18@users.noreply.github.com>
2022-03-02 12:14:48 -0500

    ID:14 - Timer
    Updated Main Menu UI and added on screen timer script

---
+++ Assets/UIScripts/UITimerScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITimerScript : MonoBehaviour
+{
+	public TMP_Text TimerText;
+	public bool playing;
+	private float Timer;
+
+	void Update()
+	{
+
+		if (playing == true)
+		{
+
+			Timer += Time.deltaTime;
+			int minutes = Mathf.FloorToInt(Timer / 60F);
+			int seconds = Mathf.FloorToInt(Timer % 60F);
+			int milliseconds = Mathf.FloorToInt((Timer * 100F) % 100F);
+			TimerText.text = minutes.ToString("00") + ":" + seconds.ToString("00") + ":" + milliseconds.ToString("00");
+		}
+
+	}
+
+}
