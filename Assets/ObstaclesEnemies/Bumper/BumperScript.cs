b4d85d303d0072e490ecf2ce4714eb0bad55a745
JamesFerchak <99982812+JamesFerchak@users.noreply.github.com>
2022-03-23 13:21:12 -0400

    Work on bumper (id#19)

--- Assets/ObstaclesEnemies/Bumper/BumperScript.cs
+++ Assets/ObstaclesEnemies/Bumper/BumperScript.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class BumperScript : MonoBehaviour
 {
-    string playerTag;
-    float bounceForce;
+    public float forceMultiplier;
+    GameObject player = References.thePlayer;
+    Vector3 knockbackVelocity;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        knockbackVelocity = new Vector3((transform.position.x - player.transform.position.x) * forceMultiplier, (transform.position.y - player.transform.position.y) * forceMultiplier, (transform.position.z - player.transform.position.z) * forceMultiplier);
+        player.GetComponent<Rigidbody>().velocity = -knockbackVelocity;
+        Debug.Log("Bounced?");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +24,6 @@ public class BumperScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
