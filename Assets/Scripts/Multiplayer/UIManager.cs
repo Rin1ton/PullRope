1a148c12ebc89e3349cce84b634b1a26d84cae3e
Vinni Horstkotte <n00i3.n4.i.0reooo@gmail.com>
2022-10-14 14:56:02 -0400

    more multiplayer implementation

--- Assets/Scripts/Multiplayer/UIManager.cs
+++ Assets/Scripts/Multiplayer/UIManager.cs
@@ -1,18 +1,55 @@
+using Riptide;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private static UIManager _singleton;
+	public static UIManager Singleton
+	{
+		get => _singleton;
+		private set
+		{
+			if (_singleton == null) _singleton = value;
+			else
+			{
+				Debug.LogWarning($"{nameof(UIManager)} instance already exists, destroying duplicate!");
+				Destroy(value);
+			}
+		}
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField] private GameObject connectUI;
+	[SerializeField] private InputField usernameField;
+
+	private void Awake()
+	{
+		Singleton = this;
+	}
+
+	public void ConnectClicked()
+	{
+		usernameField.interactable = false;
+		connectUI.SetActive(false);
+
+		NetworkManager.Singleton.Connect();
+	}
+
+	public void BackToMain()
+	{
+		usernameField.interactable = true;
+		connectUI.SetActive(true);
+	}
+
+	public void SendName()
+	{
+		Message message = Message.Create(MessageSendMode.Reliable, (ushort)ClientToServerId.name);
+		message.AddString(usernameField.text);
+		NetworkManager.Singleton.Client.Send(message);
+	}
+
+
+
 }
