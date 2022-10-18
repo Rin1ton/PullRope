1d38f73fbfc7c7358fecc3a83b9b3e4e7d70853d
Vinni Horstkotte <n00i3.n4.i.0reooo@gmail.com>
2022-10-18 14:43:13 -0400

    added sql support and playerdata struct

--- Assets/Common Scripts/References.cs
+++ Assets/Common Scripts/References.cs
@@ -9,4 +9,38 @@ public static class References
 	public static float playerSensitivityX = 1;
 	public static float playerSensitivityY = 1;
 	public static Color32 CrosshairColor;
+
+	public struct PlayerData
+	{
+		string username;
+		string password;
+		int coincount;
+		string equipped;
+		int cosmetic_copper;
+		int cosmetic_dirt;
+		int cosmetic_gold;
+		int cosmetic_grass;
+		int cosmetic_matrix;
+		int cosmetic_purple;
+		int cosmetic_sapphire;
+		int cosmetic_sus;
+	}
+
+	/*
+	string username;
+	string password;
+	int coincount;
+	string equipped;
+	int cosmetic_copper;
+	int cosmetic_dirt;
+	int cosmetic_gold;
+	int cosmetic_grass;
+	int cosmetic_matrix;
+	int cosmetic_purple;
+	int cosmetic_sapphire;
+	int cosmetic_sus;
+	*/
+
+	public static PlayerData localPlayerData;
+
 }
