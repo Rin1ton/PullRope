2b68e052a7fcbe8d9ba5f0fdd76193e815747677
JamesFerchak <99982812+JamesFerchak@users.noreply.github.com>
2022-10-18 14:54:13 -0400

    a basic skin loader is working

--- Assets/Scripts/SkinLoader.cs
+++ Assets/Scripts/SkinLoader.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class SkinLoader : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public Material skinToLoad;
+    public GameObject Object;
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Object.GetComponent<MeshRenderer>().material = skinToLoad;
     }
 }
