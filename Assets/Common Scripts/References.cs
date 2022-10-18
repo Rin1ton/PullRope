370d327b9ade88c60fcdcf1ec2cdd695602da5cc
psargent21 <99279443+psargent21@users.noreply.github.com>
2022-10-18 17:11:08 -0400

    Update References.cs

--- Assets/Common Scripts/References.cs
+++ Assets/Common Scripts/References.cs
@@ -10,26 +10,20 @@ public static class References
 	public static float playerSensitivityY = 1;
 	public static Color32 CrosshairColor;
 
-	public struct PlayerData
+	public struct localPlayerData
 	{
-		string username;
-		string password;
-		int coincount;
-		string equipped;
-		int cosmetic_copper;
-		int cosmetic_dirt;
-		int cosmetic_gold;
-		int cosmetic_grass;
-		int cosmetic_matrix;
-		int cosmetic_purple;
-		int cosmetic_sapphire;
-		int cosmetic_sus;
-	}
-
-	public struct remotePlayerData
-	{
-		string username;
-		string equipped;
+		public string username;
+		public string password;
+		public int coincount;
+		public string equipped;
+		public int cosmetic_copper;
+		public int cosmetic_dirt;
+		public int cosmetic_gold;
+		public int cosmetic_grass;
+		public int cosmetic_matrix;
+		public int cosmetic_purple;
+		public int cosmetic_sapphire;
+		public int cosmetic_sus;
 	}
 
 	/*
@@ -46,8 +40,5 @@ public static class References
 	int cosmetic_sapphire;
 	int cosmetic_sus;
 	*/
-
-	public static PlayerData localPlayerData;
-	public static List<PlayerData> remotePlayersData;
 
 }
