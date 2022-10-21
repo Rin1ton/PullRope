3202c39debea40cadd8a6b565fe9ba296f1693b3
Vinni Horstkotte <n00i3.n4.i.0reooo@gmail.com>
2022-10-21 14:09:07 -0400

    more multiplayer progress

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public static Dictionary<ushort, Player> list = new Dictionary<ushort, Player>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public ushort Id { get; private set; }
+	
+	//is this the local player?
+	public bool IsLocal { get; private set; }
+
+	private string username;
+
+	private void OnDestroy()
+	{
+		list.Remove(Id);
+	}
+
+	public static void Spawn(ushort id, string username, Vector3 vector3)
+	{
+
+	}
+
 }
