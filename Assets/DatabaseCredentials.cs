9c68c7ac7d32645f5f41c702ef8ebed3f05cd7b5
Vinni Horstkotte <n00i3.n4.i.0reooo@gmail.com>
2022-10-28 14:44:20 -0400

    test

--- Assets/DatabaseCredentials.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class DatabaseCredentials
-{
-	public static readonly string databaseUsername = "root";		//This should be the same, you shouldn't have to change it
-	public static readonly string databasePassword = "sdnaiofndsaifsaf";
-}
