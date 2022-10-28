31ac362f546e90a4a26538160ced11fd3328df89
Vinni Horstkotte <n00i3.n4.i.0reooo@gmail.com>
2022-10-28 14:16:50 -0400

    Added database credentials class

---
+++ Assets/DatabaseCredentials.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DatabaseCredentials
+{
+	public static readonly string databaseUsername = "root";		//This should be the same, you shouldn't have to change it
+	public static readonly string databasePassword = "password";
+}
