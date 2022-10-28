e25d54f67a5ea03530b28e2d7d78b08da06a60fd
Vinni Horstkotte <n00i3.n4.i.0reooo@gmail.com>
2022-10-28 14:25:12 -0400

    add databasecredentials to gitignore

--- Assets/DatabaseCredentials.cs
+++ Assets/DatabaseCredentials.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 public static class DatabaseCredentials
 {
 	public static readonly string databaseUsername = "root";		//This should be the same, you shouldn't have to change it
-	public static readonly string databasePassword = "password";
+	public static readonly string databasePassword = "nice one idiao";
 }
