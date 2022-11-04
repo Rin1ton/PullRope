0acaefed13a62072bc386cfe7e84f983930b63b5
JamesFerchak <99982812+JamesFerchak@users.noreply.github.com>
2022-11-04 14:29:07 -0400

    Cosmetic -> Skin

--- Assets/Scripts/SkinLoader.cs
+++ Assets/Scripts/SkinLoader.cs
@@ -24,56 +24,56 @@ public class SkinLoader : MonoBehaviour
         player.GetComponent<MeshRenderer>().material = skin1;
         icon.GetComponent<MeshRenderer>().material = skin1;
         References.currentSkin = skin1;
-        References.currentSkinName = "cosmetic_dirt";
+        References.currentSkinName = "skin_dirt";
     }
     public void Skin2ButtonClicked()
     {
         player.GetComponent<MeshRenderer>().material = skin2;
         icon.GetComponent<MeshRenderer>().material = skin2;
         References.currentSkin = skin2;
-        References.currentSkinName = "current_copper";
+        References.currentSkinName = "skin_copper";
     }
     public void Skin3ButtonClicked()
     {
         player.GetComponent<MeshRenderer>().material = skin3;
         icon.GetComponent<MeshRenderer>().material = skin3;
         References.currentSkin = skin3;
-        References.currentSkinName = "cosmetic_gold";
+        References.currentSkinName = "skin_gold";
     }
     public void Skin4ButtonClicked()
     {
         player.GetComponent<MeshRenderer>().material = skin4;
         icon.GetComponent<MeshRenderer>().material = skin4;
         References.currentSkin = skin4;
-        References.currentSkinName = "cosmetic_sapphire";
+        References.currentSkinName = "skin_sapphire";
     }
     public void Skin5ButtonClicked()
     {
         player.GetComponent<MeshRenderer>().material = skin5;
         icon.GetComponent<MeshRenderer>().material = skin5;
         References.currentSkin = skin5;
-        References.currentSkinName = "cosmetic_purple";
+        References.currentSkinName = "skin_purple";
     }
     public void Skin6ButtonClicked()
     {
         player.GetComponent<MeshRenderer>().material = skin6;
         icon.GetComponent<MeshRenderer>().material = skin6;
         References.currentSkin = skin6;
-        References.currentSkinName = "cosmetic_grass";
+        References.currentSkinName = "skin_grass";
     }
     public void Skin7ButtonClicked()
     {
         player.GetComponent<MeshRenderer>().material = skin7;
         icon.GetComponent<MeshRenderer>().material = skin7;
         References.currentSkin = skin7;
-        References.currentSkinName = "cosmetic_matrix";
+        References.currentSkinName = "skin_matrix";
     }
     public void Skin8ButtonClicked()
     {
         player.GetComponent<MeshRenderer>().material = skin8;
         icon.GetComponent<MeshRenderer>().material = skin8;
         References.currentSkin = skin8;
-        References.currentSkinName = "cosmetic_sus";
+        References.currentSkinName = "skin_sus";
     }
 
 }
