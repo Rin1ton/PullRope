8f24a2cd70b033e402c10238d14f1c9082f706c5
JamesFerchak <99982812+JamesFerchak@users.noreply.github.com>
2022-11-04 14:01:47 -0400

    More skin loading stuff

--- Assets/Scripts/PlayerSkinLoader.cs
+++ Assets/Scripts/PlayerSkinLoader.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PlayerSkinLoader : MonoBehaviour
 {
+    GameObject player;
+    private References.localPlayerData _myPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = References.thePlayer;
+
+
+
     }
 
     // Update is called once per frame
